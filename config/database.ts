f59622b9820114d24ebe66eaa573eb79e256b689
ToiVT-K62 <trongtoihust@gmail.com>
2025-10-23 20:34:21 +0700

    update

--- config/database.ts
+++ config/database.ts
@@ -7,7 +7,7 @@ const dbConfig = defineConfig({
     sqlite: {
       client: 'better-sqlite3',
       connection: {
-        filename: app.tmpPath('db.sqlite3')
+        filename: process.env.DB_FILENAME || '/app/database/database.sqlite',
       },
       useNullAsDefault: true,
       migrations: {
